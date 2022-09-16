5123c63e0fde7c25ab9594a5256125dd11332393
dbcr1 <eldar.kuzhekov@yandex.ru>
2022-09-16 17:45:12 +0400

    .

---
+++ lidar_v2/lidar_v2.ino
@@ -0,0 +1,79 @@
+uint8_t inByte = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(115200);
+  Serial.begin(115200);
+}
+int start_count = 0;
+uint8_t raw[1980] = {};
+void loop() {
+
+  bool got_scan = false;
+
+  int motor_speed = 0;
+  // put your main code here, to run repeatedly:
+
+  // get incoming byte:
+  Serial1.readBytes(&raw[start_count], 1);
+  if (start_count == 0) {
+    if (raw[start_count] == 0xFA) {
+      start_count = 1;
+    }
+  } else if (start_count == 1) {
+    if (raw[start_count] == 0xA0) {
+      start_count = 0;
+
+      // Now that entire start sequence has been found, read in the rest of the message
+      got_scan = true;
+      Serial1.readBytes(&raw[2], 1978);
+
+
+
+
+      Serial1.readBytes(&raw[2], 1978);
+      int index;
+      for (uint16_t i = 0; i < 1980;  i ++) {
+        if (raw[i] == 0xFA && raw[i + 1] == (0xA0 + i / 22)) { //&& CRC check
+          //Serial.println("-----------------------------------");
+          motor_speed += (raw[i + 3] << 8) + raw[i + 2]; //accumulate count for avg. time increment
+          int rpms = (raw[i + 3] << 8 | raw[i + 2]) / 64;
+          for (uint16_t j = i + 4; j < i + 20; j = j + 4) {
+
+            // Four bytes per reading
+            index = (4 * i) / 22 + (j - 4 - i) / 4;
+            uint8_t byte0 = raw[j];
+            uint8_t byte1 = raw[j + 1];
+            uint8_t byte2 = raw[j + 2];
+            uint8_t byte3 = raw[j + 3];
+            // First two bits of byte1 are status flags
+            // uint8_t flag1 = (byte1 & 0x80) >> 7;  // No return/max range/too low of reflectivity
+            // uint8_t flag2 = (byte1 & 0x40) >> 6;  // Object too close, possible poor reading due to proximity kicks in at < 0.6m
+            // Remaining bits are the range in mm
+            uint16_t range = ((byte1 & 0x3F) << 8) + byte0;
+            uint16_t intensity = (byte3 << 8) + byte2;
+            /*
+              Serial.print(byte0, HEX);
+              Serial.print('\t');
+              Serial.print(byte1, HEX);
+              Serial.print('\t');
+              Serial.print(byte2, HEX);
+              Serial.print('\t');
+              Serial.print(byte3, HEX);
+              Serial.print('\t');
+            */
+            //Serial.print(rpms);
+            //Serial.print('\t');
+            Serial.println(String(index) + ' ' + String(range / 10.0));
+            //Serial.print('\t');
+            //Serial.println(range);
+            //Serial.print('\t');
+            //Serial.println(intensity);
+          }
+        }
+        if ( i == 1979) {
+          uint8_t raw[1980] = {};
+        }
+      }
+    }
+  }
+}
---
+++ visual_lidar_xv11.py
@@ -0,0 +1,34 @@
+import serial
+import time
+import cv2 as cv
+import math
+
+lidar = serial.Serial('COM5', 115200)
+output = cv.imread(r"output.png")
+
+output = cv.resize(output, (1000, 1000))
+angle = 0
+while True:
+    r = lidar.read_until()
+    print (r)
+    try:
+        data = (str(r)[2:-5]).split()
+        angle = int(data[0])
+        leingh = float(data[1])
+
+    except:
+        leingh = 0
+
+    if leingh > 10:
+        x1 = 500 - leingh * math.cos(angle * 3.14 / 180)
+        y1 = 500 - leingh * math.sin(angle * 3.14/ 180)
+        output = cv.circle(output, (int(y1), int(x1)), 1, (255,255,255), -1)
+     
+    output = cv.circle(output, (500, 500), 2, (0,0,255), -1)
+    cv.imshow('input2', output)
+    if (angle == 359):
+        cv.rectangle(output, (0,0), (1000, 1000), (0,0,0), -1)
+    if cv.waitKey(1) == 27:
+        break
+
+lidar.close()
